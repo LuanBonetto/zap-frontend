9a173451a153b356be84abd8eb0a3f5f51d0f6c9
LuanBonetto <luanbonetto42@gmail.com>
2020-05-06 23:14:09 -0300

    criando requisição para o login

--- zap-app/src/components/LoginPage/index.js
+++ zap-app/src/components/LoginPage/index.js
@@ -14,18 +14,31 @@ import {
   buttonLink,
   buttonEnter,
 } from "./style";
-import { useDispatch } from "react-redux"
-import { push } from "connected-react-router"
+import { useDispatch } from "react-redux";
+import { push } from "connected-react-router";
+import { useState } from "react";
+import { login } from "../../actions/user"
 
 function LoginPage(props) {
-  const dispatch = useDispatch()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
 
-  function goToSignUpPage(){
-    dispatch(push("/signup"))
+  function goToSignUpPage() {
+    dispatch(push("/signup"));
   }
 
-  function goToRedefinePasswordPage(){
-    dispatch(push("/redefinePassword"))
+  function goToRedefinePasswordPage() {
+    dispatch(push("/redefinePassword"));
+  }
+
+  function sendLogin(event){
+    event.preventDefault()
+    const user = {
+      email,
+      password
+    }
+    dispatch(login(user))
   }
 
   return (
@@ -33,19 +46,35 @@ function LoginPage(props) {
       <img alt="logo" style={logo} src={zapLogo} />
       <h1 style={title}>É ruim de conversar no Messenger?</h1>
       <h1 style={title}>Então manda um Zap!</h1>
-      <Form style={form}>
+      <Form style={form} onSubmit={sendLogin}>
         <Form.Group style={formGroup}>
           <Form.Label style={label}>Email</Form.Label>
-          <Form.Control type="email" placeholder="Email" style={input} />
+          <Form.Control
+            type="email"
+            placeholder="Email"
+            style={input}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <Form.Label style={label}>Senha</Form.Label>
-          <Form.Control type="password" placeholder="Senha" style={input} />
-          <Button style={buttonLink} variant="link" onClick={goToRedefinePasswordPage}>
+          <Form.Control
+            type="password"
+            placeholder="Senha"
+            style={input}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            style={buttonLink}
+            variant="link"
+            onClick={goToRedefinePasswordPage}
+          >
             Esqueci minha senha
           </Button>
           <Button style={buttonLink} variant="link" onClick={goToSignUpPage}>
             Cadastre-se
           </Button>
-          <Button style={buttonEnter} variant="success">
+          <Button style={buttonEnter} variant="success" type="submit">
             Entrar
           </Button>
         </Form.Group>
